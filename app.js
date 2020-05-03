387d9477c417d5a6849d806e1050a00f27bf3966
lapinoux02 <paulbnt@free.fr>
2020-05-03 15:50:39 +0200

    debug en prod, yeah !

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+window.onerror = function(message, source, lineno, colno, error) {
+	alert(message)
+}
+
 // Gestion service worker
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
